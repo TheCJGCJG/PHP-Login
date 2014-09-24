85a59994489f95792ffcc1e9c59249b7ff450d5f
Matt Kent <mattkent@infinity-six.com>
2014-09-24 23:20:06 +0100

    General Code Refactoring
    I noticed that there was a mix between Object Oriented and Procedural style in the class so I made it all Object Oriented to prevent confusion.
    
    I added 3 private attributes to the top of the class; $_connection, $_last_query and $_info. In the constructor as you can see the $_info attribute is an associative array that handles all the database credentials (I added the database port). I also did the error checking when the connection is established, instead of putting it into a new method.
    
    This change means that you enter your information as arguments to the constructor like so,
    
    $login = new login('localhost', 'username', 'password', 'database', 'port', 'prefix');
    
    Everything else is the same. I did do some shuffling around and general refactoring of the code. Since I made these changes I have tested creating a user account, and it was successful. I haven't however tested any other methods but they should work.
    
    I also added a new argument to the retrieveUser method which determines whether the user retrieval is for checking if the account exists or whether to return a record from the database.

--- login.php
+++ login.php
@@ -1,19 +1,19 @@
 <?php
 /**
-* A basic login class - Allows for MySQL Storage of Usernames & Hashed 
-* Passwords. Performs basic input validation to prevent against SQL
-* Injection, however does no more. The user is expected to protect against
-* these types of attacks
-*
-* LICENSE: This source file is subject to version 3.01 of the PHP license
-* that is available through the world-wide-web at the following URI:
-* http://www.php.net/license/3_01.txt.  If you did not receive a copy of
-* the PHP License and are unable to obtain it through the web, please
-* send a note to license@php.net so we can mail you a copy immediately.
-*
-* @author Charles Gillham <charlesg.github@gmail.com>
-*
-*/
+ * A basic login class - Allows for MySQL Storage of Usernames & Hashed
+ * Passwords. Performs basic input validation to prevent against SQL
+ * Injection, however does no more. The user is expected to protect against
+ * these types of attacks
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @author Charles Gillham <charlesg.github@gmail.com>
+ *
+ */
 
 /*
 * mysql_host
@@ -22,144 +22,152 @@
 * mysql_database
 * mysql_login_prefix
 */
+
 class login
 
 {
-	function init($host, $user, $password, $db, $prefix)
-	{
-		define("mysql_host", $host);
-		define("mysql_user", $user);
-		define("mysql_password", $password);
-		define("mysql_database", $db);
-		define("mysql_login_prefix", $prefix);
-		if ($this::testSql() == true) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+    private $_connection;
+    private $_last_query;
+    private $_info;
 
-	static
-	function sqlQuery($query)
-	{
-		$sql = mysqli_connect(mysql_host, mysql_user, mysql_password, mysql_database);
-		$result = mysqli_query($sql, $query);
-		return $result;
-	}
+    public function __construct($server, $user, $pass, $database, $port, $prefix)
+    {
+        $this->_info = array(
+            'Server' => $server,
+            'Username' => $user,
+            'Password' => $pass,
+            'Database' => $database,
+            'Port'     => $port,
+            'Prefix'   => $prefix
+        );
 
-	function testSql()
-	{
-		if (mysqli_connect_errno()) {
-			die("Failed to connect to MySQL: " . mysqli_connect_error());
-		}
+        $this->_connection = new mysqli(
+            $this->_info['Server'],
+            $this->_info['Username'],
+            $this->_info['Password'],
+            $this->_info['Database'],
+            $this->_info['Port']
+        );
 
-		if (mysqli_num_rows($this::sqlQuery("SHOW TABLES LIKE '" . mysql_login_prefix . "accounts'")) == 1) {
-		}
-		else {
-			if ($this::sqlQuery("CREATE TABLE `sshkeys`.`" . mysql_login_prefix . "accounts`(`ID` INT NOT NULL AUTO_INCREMENT,`username` VARCHAR(45)NOT NULL,`password` VARCHAR(150)NOT NULL,PRIMARY KEY(`ID`));")) {
-			}
-			else {
-				die("An SQL Error occurred");
-			}
-		}
+        if ($this->_connection->connect_error)
+        {
+            exit('Error connecting to MySQL: ' . $this->_connection->connect_error);
+        }
 
-		return true;
-	}
+        return $this->_info;
+    }
 
-	static
-	function hashPassword($password)
-	{
-		$hashed = password_hash($password, PASSWORD_DEFAULT);
-		return $hashed;
-	}
+    public function Connection()
+    {
+        return $this->_connection;
+    }
 
-	function createAccount($username, $password)
-	{
-		$sql = mysqli_connect(mysql_host, mysql_user, mysql_password, mysql_database);
-		$username = mysqli_real_escape_string($sql, $username);
-		$password = $this::hashPassword($password);
-		if ($this::retrieveUser($username)['status'] == true) {
-			return false;
-		}
-		$query = "INSERT INTO " . mysql_login_prefix . "accounts (username, password) VALUES ('" . $username . "', '" . $password . "');";
-		if ($this::sqlQuery($query)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public function sqlQuery($sql)
+    {
+        $this->_last_query = $sql;
+        $result = $this->Connection()->query($sql);
+        return $result;
+    }
 
-	function retrieveUser($username)
-	{
-		$sql = mysqli_connect(mysql_host, mysql_user, mysql_password, mysql_database);
-		$username = mysqli_real_escape_string($sql, $username);
-		$query = "SELECT * FROM " . mysql_login_prefix . "accounts WHERE username='" . $username . "';";
-		$result = $this::sqlQuery($query);
-		$userinfo = Array();
-		if (mysqli_num_rows($result) == 0) {
-			$userinfo['status'] = false;
-		}
-		else {
-			while ($row = mysqli_fetch_array($result)) {
-				$username = $row['username'];
-				$password = $row['password'];
-			}
 
-			$userinfo['status'] = true;
-			$userinfo['username'] = $username;
-			$userinfo['password'] = $password;
-		}
+    public function hashPassword($password)
+    {
+        $hashed = password_hash($password, PASSWORD_DEFAULT);
+        return $hashed;
+    }
 
-		return $userinfo;
-	}
+    public function createAccount($username, $password)
+    {
+        $username = $this->Connection()->real_escape_string($username);
+        $password = $this->Connection()->real_escape_string($this->hashPassword($password));
 
-	function checkPassword($username, $password)
-	{
-		$userinfo = $this::retrieveUser($username);
-		if ($userinfo['status'] == false) {
-			die('The given username could not be found');
-		}
 
-		$check = password_verify($password, $userinfo['password']);
-		if ($check) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	function deleteAccount($username)
-	{
-		$userinfo = $this::retrieveUser($username);
-		if ($userinfo['status'] == false) {
-			return false;
-		}
-		$sql = mysqli_connect(mysql_host, mysql_user, mysql_password, mysql_database);
-		$username = mysqli_real_escape_string($sql, $username);
-		$query = "DELETE FROM " . mysql_login_prefix . "accounts WHERE username='" . $username . "';";
-		if ($result = $this::sqlQuery($query)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	function changePassword($username, $password) {
-		$userinfo = $this::retrieveUser($username);
-		if ($userinfo['status'] == false) {
-			return false;
-		}
-		$sql = mysqli_connect(mysql_host, mysql_user, mysql_password, mysql_database);
-		$username = mysqli_real_escape_string($sql, $username);
-		$newPassword = $this::hashPassword($password);
-		$query = "UPDATE " . mysql_login_prefix . "accounts SET password='" . $newPassword . "' WHERE username='" . $username . "';";
-		echo $query;
-		if ($result = $this::sqlQuery($query)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        $userCheck = $this->retrieveUser($username, TRUE);
+        if ($userCheck['status'] == TRUE)
+        {
+            return FALSE;
+        }
+
+        $result = $this->sqlQuery("INSERT INTO " . $this->_info['Prefix'] . "accounts (username, password) VALUES ('$username', '$password')");
+
+        if ($result)
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function retrieveUser($username, $createAccount = NULL)
+    {
+        if ($createAccount == TRUE)
+        {
+        $username = $this->Connection()->real_escape_string($username);
+        $query = "SELECT * FROM " . $this->_info['Prefix'] . "accounts WHERE username= '$username';";
+        $result = $this->sqlQuery($query);
+        $userinfo = array();
+        if ($result->num_rows == 0) {
+            $userinfo['status'] = false;
+          }
+            return $userinfo;
+        }
+
+            while ($row = $result->fetch_array()) {
+                $username = $row['username'];
+                $password = $row['password'];
+            }
+
+            $userinfo['status'] = true;
+            $userinfo['username'] = $username;
+            $userinfo['password'] = $password;
+
+        return $userinfo;
+    }
+
+    public function checkPassword($username, $password)
+    {
+        $userinfo = $this->retrieveUser($username);
+        if ($userinfo['status'] == false) {
+            die('The given username could not be found');
+        }
+
+        $check = password_verify($password, $userinfo['password']);
+        if ($check) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public function deleteAccount($username)
+    {
+        $userinfo = $this->retrieveUser($username);
+        if ($userinfo['status'] == false) {
+            return false;
+        }
+        $username = $this->Connection()->real_escape_string($username);
+        $query = "DELETE FROM " . $this->_info['Prefix'] . "accounts WHERE username='$username';";
+        if ($result = $this->sqlQuery($query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function changePassword($username, $password) {
+        $userinfo = $this->retrieveUser($username);
+        if ($userinfo['status'] == false) {
+            return false;
+        }
+        $username = $this->Connection()->real_escape_string($username);
+        $newPassword = $this->hashPassword($password);
+        $query = "UPDATE " . $this->_info['Prefix'] . "accounts SET password='$newPassword' WHERE username='$username';";
+        echo $query;
+        if ($result = $this->sqlQuery($query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
-
-?>
